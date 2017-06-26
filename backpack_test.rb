11668ba0929e9589354509cb14fe11340278dae1
Ronen Annason <ronenannason83@gmail.com>
2017-06-26 12:08:00 -0400

    Added test methods About Gym Shoes to project test file.

--- backpack_test.rb
+++ backpack_test.rb
@@ -45,6 +45,20 @@ class BackpackTest < MiniTest::Test
 
   ###### TESTS ABOUT GYM SHOES... wait, where are the tests about gym shoes? #####
   # is something missing here?
+  def test_that_packs_gym_shoes_on_mondays_and_thursdays
+    ['monday', 'thursday'].each do |day|
+      backpack = build(:backpack, day_of_week: day)
+      assert(backpack.items.include?('gym shoes'))
+    end
+  end
+
+  def test_that_doesnt_pack_gym_shoes_on_non_gym_days
+    ['tuesday', 'wednesday', 'friday', 'saturday', 'sunday'].each do |day|
+      backpack = build(:backpack, day_of_week: day)
+      refute(backpack.items.include?('gym shoes'))
+    end
+  end
+  
 
 
   ##### TESTS ABOUT PRINTING THE LIST #####
