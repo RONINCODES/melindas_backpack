7c10dbb3d6dc120546c30a112febe420ac005b6b
Ronen Annason <ronenannason83@gmail.com>
2017-06-26 15:03:47 -0400

    simplified prepare method by replacing blocks of code for packing for rainy day and packing for cold day - by creating seperate methods

--- backpack.rb
+++ backpack.rb
@@ -17,23 +17,27 @@ class Backpack
     # Ensure appropriate clothing is added to backpack
     pack_essential_clothing
     pack_gym_shoes_on_gym_days
+    pack_lunch_not_on_weekend
+    rainy_day
+    cold_day
+  end
+
+
+  def rainy_day
+    current_weather = @attributes[:weather]
     if current_weather == 'rainy'
-      @items << 'umbrella'
-    elsif current_weather == 'cold'
-      @items << 'jacket'
-    else
-    end
+    @items << 'umbrella'
+   end
+ end
 
 
-
-    # Bring a packed lunch on all weekdays
-
-    if day_of_week != 'saturday' && day_of_week != 'sunday'
-      @items << 'packed lunch'
-    elsif false
-      @items << 'snacks'
+  def cold_day
+    current_weather = @attributes[:weather]
+    if current_weather == 'cold'
+    @items << 'jacket'
     end
   end
+
 
   def pack_gym_shoes_on_gym_days
     # Ensure gym shoes are added to backpack if it's a gym day
@@ -43,7 +47,15 @@ class Backpack
     end
   end
 
-
+  def pack_lunch_not_on_weekend
+    # Bring a packed lunch on all weekdays
+    day_of_week = @attributes[:day_of_week]
+    if day_of_week != 'saturday' && day_of_week != 'sunday'
+      @items << 'packed lunch'
+    elsif false
+      @items << 'snacks'
+    end
+  end
 
   def pack_essential_clothing
     @items << 'pants'
